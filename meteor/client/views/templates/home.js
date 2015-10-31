1f4ecbf5b52a26c753e65a57d60e19dcee8d6077
Connor Hallett <connor.hallett@gmail.com>
2015-10-30 23:18:27 -0400

    Example workflow with ionic

---
+++ meteor/client/views/templates/home.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Connor on 10/30/2015.
+ */
+Template.home.helpers({
+    items: [
+        {
+            _id: '1',
+            name: 'test',
+            price: 10
+        },
+        {
+            _id: '5',
+            name: 'test',
+            price: 10
+        },
+        {
+            _id: '4',
+            name: 'test',
+            price: 10
+        },
+        {
+            _id: '3',
+            name: 'test',
+            price: 10
+        },
+        {
+            _id: '2',
+            name: 'test',
+            price: 10
+        },
+    ]
+});
---
+++ meteor/routes/detailRoute.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Connor on 10/30/2015.
+ */
+/**
+ * Created by Connor on 10/30/2015.
+ */
+DetailsController = RouteController.extend({
+    template: 'detail',
+    waitOn: function() {
+    },
+    data: function() {
+    },
+    before: function () {
+        this.next();
+    }
+});
+
+Router.route('/:_id', {
+    name: 'detail',
+    controller: DetailsController
+});
