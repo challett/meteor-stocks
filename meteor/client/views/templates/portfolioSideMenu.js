63ecde3972b4ba401db957e131ff55a0d35bb4e3
Connor Hallett <connor.hallett@gmail.com>
2015-12-02 22:28:08 -0500

    Added options to sidebar

--- meteor/client/views/templates/portfolioSideMenu.js
+++ meteor/client/views/templates/portfolioSideMenu.js
@@ -1,3 +1,30 @@
 /**
  * Created by Connor on 11/26/2015.
  */
+Template.portfolioSideMenu.events({
+    'click .item-checkbox': function (event) {
+        console.log(this)
+    }
+});
+
+Template.portfolioSideMenu.helpers({
+    'stockInfo': function () {
+        var stock = Stocks.findOne();
+        console.log(lodash.keys(stock))
+        return lodash.keys(stock);
+    },
+    'weHaveData': function (key) {
+        var stock = Stocks.findOne(),
+            exclude = [
+                '_id'
+            ];
+        return stock[key] && lodash.indexOf(exclude, key) === -1
+    },
+    'checked': function (key) {
+        return Session.equals(key , true)
+    },
+    preference: function () {
+        return Preferences.find();
+    },
+
+});
