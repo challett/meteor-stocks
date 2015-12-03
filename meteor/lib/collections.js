b04e14bb7a17877a0b6b61fd3ccfd36feeff7bdd
Spencer Lee <spence1994@hotmail.com>
2015-12-03 18:19:30 -0500

    Stock symbols

--- meteor/lib/collections.js
+++ meteor/lib/collections.js
@@ -1,4 +1,15 @@
 /**
  * Created by Connor on 11/26/2015.
  */
+//var results = Papa.parse('/symbols.csv', {
+//    download: true,
+//    header: true,
+//    complete: function(results) {
+//        console.log(results);
+//        lodash.forEach(results.data, function (value){
+//            Stocks.upsert({symbol: value.symbol},{$set:{symbol: value.symbol}});
+//        });
+//    }
+//});
 Stocks = new Meteor.Collection('stocks');
+
