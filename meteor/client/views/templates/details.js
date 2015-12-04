28b598b84a2c9a648d823e747fc9840cf6f5aeeb
Nicolas Lelievre <lelievnm@mcmaster.ca>
2015-12-03 23:12:16 -0500

    UI design improvements

--- meteor/client/views/templates/details.js
+++ meteor/client/views/templates/details.js
@@ -10,9 +10,10 @@ Template.detail.helpers({
 
 Template.detail.rendered = function () {
     Meteor.call('getHistoricalData', {symbol: this.data.stock.symbol}, function (err, data) {
-        var margin = {top: 20, right: 20, bottom: 30, left: 50},
-            width = 550,
-            height = 500;
+        var margin = {top: 0, right: 0, bottom: 0, left: 0},
+            width = document.getElementById("graph-card-section").clientWidth,
+            height = document.getElementById("graph-card-section").clientHeight;
+            xAxisHeight = height - 32;
 
         var parseDate = d3.time.format("%d-%b-%y").parse;
 
@@ -34,11 +35,37 @@ Template.detail.rendered = function () {
             .x(function(d) { return x(d.date); })
             .y(function(d) { return y(d.close); });
 
+        var area = d3.svg.area()
+            .x(function(d) { return x(d.date); })
+            .y0(height)
+            .y1(function(d) { return y(d.close); });
+
+        var valueline = d3.svg.line()
+            .x(function(d) { return x(d.date); })
+            .y(function(d) { return y(d.close); });
+
+        // function for the x grid lines
+        /*function make_x_axis() {
+            return d3.svg.axis()
+                .scale(x)
+                .orient("bottom")
+                .ticks(0)
+        }*/
+
+        // function for the y grid lines
+        /*function make_y_axis() {
+            return d3.svg.axis()
+                .scale(y)
+                .orient("left")
+                .ticks(0)
+        }*/
+
         var svg = d3.select("#graph").append("svg")
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
         data.forEach(function(d) {
             d.date = parseDate(moment(d.date).format('DD-MMM-YY'));
             d.close = +d.close;
@@ -47,25 +74,72 @@ Template.detail.rendered = function () {
         x.domain(d3.extent(data, function(d) { return d.date; }));
         y.domain(d3.extent(data, function(d) { return d.close; }));
 
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
-
-        svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxis)
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Price ($)");
-
         svg.append("path")
             .datum(data)
             .attr("class", "line")
             .attr("d", line);
+
+        // Add the filled area
+        svg.append("path")
+            .datum(data)
+            .attr("class", "area")
+            .attr("d", area);
+
+        // Draw the x Grid lines
+        /*svg.append("g")
+            .attr("class", "grid")
+            .attr("transform", "translate(0," + height + ")")
+            .call(make_x_axis()
+                .tickSize(-height, 0, 0)
+                .tickFormat("")
+        )*/
+
+        // Draw the y Grid lines
+        /*svg.append("g")
+            .attr("class", "grid")
+            .call(make_y_axis()
+                .tickSize(-width, 0, 0)
+                .tickFormat("")
+        )*/
+
+        // Add the valueline path.
+        svg.append("path")
+            .attr("d", valueline(data));
+
+        // Add the X Axis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(-20," + xAxisHeight + ")")
+            .call(xAxis);
+
+        // Add the Y Axis
+        svg.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(35,0)")
+            .attr("x", margin.top - (height / 2))
+            .attr("dy", ".71em")
+            .call(yAxis);
+
+        // Add the white background to the y axis label for legibility
+        /*svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("x", margin.top - (height / 2))
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .attr("class", "shadow")
+            .text("Price ($)");*/
+
+        // Add the text label for the Y axis
+        /*svg.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("x", margin.top - (height / 2))
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Price ($)");*/
+
+
     });
 
 };
